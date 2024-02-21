2a455277500007b84e23c44090ccb8400991b387
AleshaFozu <lexalis12@mail.ru>
2024-02-21 21:39:41 +0700

    12
    Added Close button
    Added bazzed gui

--- GoGO/MyForm.h
+++ GoGO/MyForm.h
@@ -34,6 +34,8 @@ namespace GoGO {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^ button1;
+	protected:
 
 	private:
 		/// <summary>
@@ -49,13 +51,33 @@ namespace GoGO {
 		void InitializeComponent(void)
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
+			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->button1->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15));
+			this->button1->ForeColor = System::Drawing::Color::OrangeRed;
+			this->button1->Location = System::Drawing::Point(960, 3);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(34, 32);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"X";
+			this->button1->UseVisualStyleBackColor = false;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(284, 261);
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->ClientSize = System::Drawing::Size(997, 615);
+			this->Controls->Add(this->button1);
+			this->ForeColor = System::Drawing::SystemColors::ButtonShadow;
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
 			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
 			this->Name = L"MyForm";
 			this->Text = L"GoGO";
@@ -65,6 +87,9 @@ namespace GoGO {
 		}
 #pragma endregion
 	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->Close();
 	}
 	};
 }
