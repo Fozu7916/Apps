5165c012c70266ffd89abacfdbb1da8fcc7caa1f
AleshaFozu <lexalis12@mail.ru>
2024-02-23 01:33:39 +0700

    s
    fixed files

--- GoGO/MyForm.cpp
+++ GoGO/MyForm.cpp
@@ -1,13 +1,84 @@
 #include "MyForm.h"
-
+#include "time.h"
 
 #include <Windows.h>
 
 using namespace GoGO;
+using namespace System;
+using namespace System::Windows::Forms;
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     Application::EnableVisualStyles();
     Application::SetCompatibleTextRenderingDefault(false);
     Application::Run(gcnew MyForm);
     return 0;
+    srand(time(NULL));
 }
+
+System::Void GoGO::MyForm::close_button_Click(System::Object^ sender, System::EventArgs^ e)
+{
+    this->Close();
+};
+
+System::Void GoGO::MyForm::start_button_Click(System::Object^ sender, System::EventArgs^ e)
+{
+    int CoountryNumber = 0;
+    int country_count = 12;
+    CoountryNumber = rand()% country_count;
+
+    switch (CoountryNumber)
+    {
+    case 1:
+        Result->Visible = true;
+        Result->Text = "Канада";
+        break;
+    case 2:
+        Result->Visible = true;
+        Result->Text = "CCCР";
+        break;
+    case 3:
+        Result->Visible = true;
+        Result->Text = "США";
+        break;
+    case 4:
+        Result->Visible = true;
+        Result->Text = "Великобритания";
+        break;
+    case 5:
+        Result->Visible = true;
+        Result->Text = "Германия";
+        break;
+    case 6:
+        Result->Visible = true;
+        Result->Text = "Венгрия";
+        break;
+    case 7:
+        Result->Visible = true;
+        Result->Text = "Румыния";
+        break;
+    case 8:
+        Result->Visible = true;
+        Result->Text = "Италия";
+        break;
+    case 9:
+        Result->Visible = true;
+        Result->Text = "Турция";
+        break;
+    case 10:
+        Result->Visible = true;
+        Result->Text = "Япония";
+        break;
+    case 11:
+        Result->Visible = true;
+        Result->Text = "Франция";
+        break;
+    case 12:
+        Result->Visible = true;
+        Result->Text = "Польшая";
+        break;
+    default:
+       break;
+    }
+   
+    
+};
--- GoGO/MyForm.h
+++ GoGO/MyForm.h
@@ -34,6 +34,25 @@ namespace GoGO {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^ close_button;
+	protected:
+
+	protected:
+
+	protected:
+
+	private: System::Windows::Forms::Label^ Sigma_label;
+	private: System::Windows::Forms::Label^ zagolovok;
+
+	private: System::Windows::Forms::CheckBox^ randombox;
+
+
+	private: System::Windows::Forms::Label^ Result;
+	private: System::Windows::Forms::Button^ start_button;
+
+
+
+	protected:
 
 	private:
 		/// <summary>
@@ -49,22 +68,119 @@ namespace GoGO {
 		void InitializeComponent(void)
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
+			this->close_button = (gcnew System::Windows::Forms::Button());
+			this->Sigma_label = (gcnew System::Windows::Forms::Label());
+			this->zagolovok = (gcnew System::Windows::Forms::Label());
+			this->randombox = (gcnew System::Windows::Forms::CheckBox());
+			this->Result = (gcnew System::Windows::Forms::Label());
+			this->start_button = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
+			// 
+			// close_button
+			// 
+			this->close_button->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->close_button->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->close_button->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->close_button->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15));
+			this->close_button->ForeColor = System::Drawing::Color::OrangeRed;
+			this->close_button->Location = System::Drawing::Point(935, 3);
+			this->close_button->Name = L"close_button";
+			this->close_button->Size = System::Drawing::Size(58, 33);
+			this->close_button->TabIndex = 0;
+			this->close_button->Text = L"X";
+			this->close_button->UseVisualStyleBackColor = false;
+			this->close_button->Click += gcnew System::EventHandler(this, &MyForm::close_button_Click);
+			// 
+			// Sigma_label
+			// 
+			this->Sigma_label->AutoSize = true;
+			this->Sigma_label->BackColor = System::Drawing::Color::Black;
+			this->Sigma_label->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->Sigma_label->ForeColor = System::Drawing::SystemColors::ControlLightLight;
+			this->Sigma_label->Location = System::Drawing::Point(916, 581);
+			this->Sigma_label->Name = L"Sigma_label";
+			this->Sigma_label->Size = System::Drawing::Size(77, 25);
+			this->Sigma_label->TabIndex = 1;
+			this->Sigma_label->Text = L"Sigma";
+			// 
+			// zagolovok
+			// 
+			this->zagolovok->AutoSize = true;
+			this->zagolovok->BackColor = System::Drawing::Color::Black;
+			this->zagolovok->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 21.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->zagolovok->ForeColor = System::Drawing::SystemColors::ControlLight;
+			this->zagolovok->Location = System::Drawing::Point(262, 26);
+			this->zagolovok->Name = L"zagolovok";
+			this->zagolovok->Size = System::Drawing::Size(513, 33);
+			this->zagolovok->TabIndex = 2;
+			this->zagolovok->Text = L"Выбор Страны для ШедевроХойки";
+			// 
+			// randombox
+			// 
+			this->randombox->AutoSize = true;
+			this->randombox->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->randombox->ForeColor = System::Drawing::SystemColors::ControlText;
+			this->randombox->Location = System::Drawing::Point(40, 91);
+			this->randombox->Name = L"randombox";
+			this->randombox->Size = System::Drawing::Size(235, 24);
+			this->randombox->TabIndex = 3;
+			this->randombox->Text = L"Рандомить ли идеологию\?";
+			this->randombox->UseVisualStyleBackColor = true;
+			// 
+			// Result
+			// 
+			this->Result->AutoSize = true;
+			this->Result->BackColor = System::Drawing::Color::Black;
+			this->Result->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 21.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->Result->ForeColor = System::Drawing::SystemColors::ControlLight;
+			this->Result->Location = System::Drawing::Point(211, 474);
+			this->Result->Name = L"Result";
+			this->Result->Size = System::Drawing::Size(64, 33);
+			this->Result->TabIndex = 4;
+			this->Result->Text = L"хуй";
+			this->Result->Visible = false;
+			// 
+			// start_button
+			// 
+			this->start_button->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->start_button->ForeColor = System::Drawing::SystemColors::ControlText;
+			this->start_button->Location = System::Drawing::Point(40, 148);
+			this->start_button->Name = L"start_button";
+			this->start_button->Size = System::Drawing::Size(162, 35);
+			this->start_button->TabIndex = 5;
+			this->start_button->Text = L"НА БЕРЛИН!";
+			this->start_button->UseVisualStyleBackColor = true;
+			this->start_button->Click += gcnew System::EventHandler(this, &MyForm::start_button_Click);
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(284, 261);
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->ClientSize = System::Drawing::Size(997, 615);
+			this->Controls->Add(this->start_button);
+			this->Controls->Add(this->Result);
+			this->Controls->Add(this->randombox);
+			this->Controls->Add(this->zagolovok);
+			this->Controls->Add(this->Sigma_label);
+			this->Controls->Add(this->close_button);
+			this->ForeColor = System::Drawing::SystemColors::ButtonShadow;
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
 			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
 			this->Name = L"MyForm";
 			this->Text = L"GoGO";
-			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
-	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
-	}
-	};
+	
+	private: System::Void close_button_Click(System::Object^ sender, System::EventArgs^ e);
+    private: System::Void start_button_Click(System::Object^ sender, System::EventArgs^ e);
+};
 }
